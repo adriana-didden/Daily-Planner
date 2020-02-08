0b6b5fda5696de439f71821d898cbb48b712d8bb
Adriana Didden <adrianadidden@Adrianas-MacBook-Pro.local>
2020-02-08 17:38:47 -0500

    added ids and got current time function working properly

--- script.js
+++ script.js
@@ -5,8 +5,15 @@ var currentHour = new Date().getHours(); //Current hour in military time`
 $(document).ready(function () {
 
     function getDayTime() {
-        $("#currentDay").text = (currentDateAndTime);
+        $("#currentDay").text(currentDateAndTime);
     }
     getDayTime();
 
+    function currentTime(){
+        if(currentHour === i){
+            console.log("here")
+            $("#textArea"+i).addClass('present')
+        }
+    }
+    currentTime();
 })
