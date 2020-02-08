38ec54c57e6eca3f6deb6c7ae4f39f2ffe335bd8
Adriana Didden <adrianadidden@Adrianas-MacBook-Pro.local>
2020-02-08 15:17:11 -0500

    started building day and time function

--- script.js
+++ script.js
@@ -2,3 +2,11 @@
 var currentDateAndTime = Date(Date.now());  // exp: Fri Oct 25 2019 17:40:56 GMT-0400 (Eastern Daylight Time)`
 var currentHour = new Date().getHours(); //Current hour in military time`
 
+$(document).ready(function () {
+
+    function getDayTime() {
+        $("#currentDay").text = (currentDateAndTime);
+    }
+    getDayTime();
+
+})
