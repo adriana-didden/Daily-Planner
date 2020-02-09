9e6bd8bce0420f36559f5c0c7e44d4ce2ea6cb06
Adriana Didden <adrianadidden@Adrianas-MacBook-Pro.local>
2020-02-09 17:56:08 -0500

    added pastpresentfuture function, only past and future works.

--- script.js
+++ script.js
@@ -2,6 +2,9 @@
 var currentDateAndTime = Date(Date.now());  // exp: Fri Oct 25 2019 17:40:56 GMT-0400 (Eastern Daylight Time)`
 var currentHour = new Date().getHours(); //Current hour in military time`
 
+var num = ["9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19"];
+// var userInput = [""];
+
 $(document).ready(function () {
 
     function getDayTime() {
@@ -9,11 +12,33 @@ $(document).ready(function () {
     }
     getDayTime();
 
-    function currentTime(){
-        if(currentHour === i){
-            console.log("here")
-            $("#textArea"+i).addClass('present')
+    function pastPresentFuture() {
+        var list = [];
+        for (var i = 0; i <= num.length; i++) {
+            if (currentHour === (num[i])) {
+                console.log("here")
+                $("#textarea" + (num[i])).addClass('present')
+            } else if (currentHour > (num[i])) {
+                console.log("bye")
+                $("#textarea" + (num[i])).addClass('past')
+            } else if (currentHour < (num[i])) {
+                console.log("notyet")
+                $("#textarea" + (num[i])).addClass('future')
+            }
         }
+    };
+    pastPresentFuture();
+
+    function event() {
+        event.addEventListener("input", function () {
+            localStorage.setItem(".textarea", event.value);
+        }, false);
     }
-    currentTime();
+    function save() {
+
+
+    }
+
+
+
 })
