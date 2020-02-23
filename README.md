8995cfe1911339c631a909e76c0692f284ff7c54
adriana-didden <adrianadidden@Adrianas-MacBook-Pro.local>
2020-02-23 15:34:49 -0500

    removed some commented out content

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # daily planner read me
 
-    japplication using javascript, css, html, jquery, to build a daily planner from 9-5. backgrounds change based on the currenttime to represent past, present and future. 
+    application using javascript, css, html, jquery, to build a daily planner from 9-5. backgrounds change based on the currenttime to represent past, present and future. 
 
-![image of deployed application](./assets/images/calendar.PNG)  
+[image of deployed application](./assets/images/calendar.PNG)  
 [link to deployed site](https://adriana-didden.github.io/Daily-Planner/)
---
+++ script.js
@@ -0,0 +1,33 @@
+
+var currentDateAndTime = Date(Date.now());  // exp: Fri Oct 25 2019 17:40:56 GMT-0400 (Eastern Daylight Time)`
+var currentHour = new Date().getHours(); //Current hour in military time`
+
+var num = ["9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19"];
+// var eventInput=document.querySelector("eventText")
+$(document).ready(function () {
+
+    function getDayTime() {
+        $("#currentDay").text(currentDateAndTime);
+    }
+    getDayTime();
+
+    function pastPresentFuture() {
+        var list = [];
+        for (var i = 0; i <= num.length; i++) {
+            if (currentHour === (num[i])) {
+                console.log("here")
+                $("#textarea" + (num[i])).addClass('present')
+
+            } else if (currentHour > (num[i])) {
+                console.log("bye")
+                $("#textarea" + (num[i])).addClass('past')
+                
+            } else if (currentHour < (num[i])) {
+                console.log("notyet")
+                $("#textarea" + (num[i])).addClass('future')
+            }
+        }
+    };
+    pastPresentFuture();
+
+})
